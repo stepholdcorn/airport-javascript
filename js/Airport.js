fa6062a485a3b2eef64a50a5ff291157cdc52693
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-30 16:41:34 +0000

    Allows airport to park a plane if it has a space

--- js/Airport.js
+++ js/Airport.js
@@ -1,3 +1,13 @@
 var Airport = function() {
-  this.planeCount = []
+  this.capacity = 3;
+  this.planeCount = [];
+};
+
+Airport.prototype.parkPlane = function(plane) {
+  if (this.planeCount.length < 3) {
+    this.planeCount.push(plane);
+  }
+  else {
+    throw new Error ('All the parking spaces are taken!');
+  };
 };
--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -4,10 +4,27 @@ describe('Airport', function() {
 
   beforeEach(function() {
     airport = new Airport;
+    plane = jasmine.createSpyObj('plane', ['land']);
   });
 
   it('should have a plane count of zero by default', function() {
     expect(airport.planeCount.length).toEqual(0);
+  });
+
+  it('should have a capacity of 3 planes', function() {
+    expect(airport.capacity).toEqual(3);
+  });
+
+  it('should be able to park a plane', function() {
+    airport.parkPlane(plane);
+    expect(airport.planeCount.length).toEqual(1);
+  });
+
+  it('should not park a plane if it is full', function() {
+    airport.parkPlane();
+    airport.parkPlane();
+    airport.parkPlane();
+    expect(function() { airport.parkPlane() }).toThrow(new Error ('All the parking spaces are taken!'));
   });
 
 });
